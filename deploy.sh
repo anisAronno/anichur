9df52c2b4e779a809ffcbc63f0214012a588b198
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-09-08 14:20:24 +0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 # abort on errors
-set -e
-# build
-yarn build
+set -e 
 # navigate into the build output directory
 cd .
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-# git init
+
 git add -A
 git commit -m 'deploy'
-git push origin master
-git subtree push --prefix dist origin gh-pages
+git checkout master
+git push origin master 
 cd -
