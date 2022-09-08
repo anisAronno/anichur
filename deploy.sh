62ec335f9add80c1e921f88c575aa85191e7dbff
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-09-08 15:23:52 +0600

    sda

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+yarn build
+# navigate into the build output directory
+cd . 
+
+git add -A
+git commit -m 'deploy'
+git checkout master
+# git push origin master
+git subtree push --prefix dist origin gh-pages
+
+cd -
